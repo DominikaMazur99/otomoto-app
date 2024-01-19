f8fd5ae02e4576c15a706c63ebd3bb0c5ba57972
Dominika <dominika_mazur99@wp.pl>
2024-01-19 14:42:17 +0100

    postgres and docker works without loaded data

--- backend/ci/dockerfiles/otomoto.Dockerfile
+++ backend/ci/dockerfiles/otomoto.Dockerfile
@@ -2,13 +2,24 @@ FROM python:3.12-slim
 
 WORKDIR /app
 
+
+# Install build dependencies for psycopg2
+RUN apt-get update && apt-get install -y libpq-dev gcc
+
+# Copy requirements file separately to leverage Docker cache
 COPY ci/requirements/requirements.txt .
 
+# Install psycopg2-binary
+RUN pip install psycopg2-binary==2.9.9  # Use the desired version
 
+# Install other dependencies
 RUN pip install -r requirements.txt
 
+# Install tree
+RUN apt-get update && apt-get install -y tree
 
-
-RUN apt update && apt install -y tree
+# Copy the rest of the application code
 COPY . .
+
+# Set the entrypoint
 ENTRYPOINT ["./manage.py", "runserver", "0.0.0.0:8000"]
--- backend/otomoto/settings.py
+++ backend/otomoto/settings.py
@@ -75,9 +75,17 @@ WSGI_APPLICATION = 'otomoto.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'otomoto',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',  # This should match the service name in your Docker Compose file
+        'PORT': '5432',  # PostgreSQL default port
     }
 }
 
